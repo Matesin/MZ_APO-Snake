b61a5850fd2c0fb827998be731a7d7a04a8888a5
Matyáš Martan <martama1@fel.cvut.cz>
2024-04-23 18:37:45 +0200

    added stuff

--- main.c
+++ main.c
@@ -26,6 +26,70 @@
 #include "mzapo_regs.h"
 #include "serialize_lock.h"
 
+#define COLOR uint16_t
+#define BOOL uint8_t
+
+#define RGB(r, g, b) ((COLOR) (((r) & 0x1F) << 11) | (((g) & 0x3F) << 5) | ((b) & 0x1F))
+#define RED(c) (((r) >> 11) & 0x1F)
+#define GREEN(c) (((r) >> 5) & 0x3F)
+#define BLUE(c) (((r) >> 0) & 0x1F)
+ 
+#define NO_ERROR 0
+#define ERR_NO_MEM 1
+#define ERR_INVALID_ARGUMENT 2
+#define ERR_REINIT 3
+ 
+#define LCD_WIDTH 480
+#define LCD_HEIGHT 320
+#define FRAMEBUFFER_IDX(x, y) ((y) * LCD_WIDTH + (x))
+
+typedef struct display_t {
+  COLOR *frameBuffer = 0;
+  unsigned char* parlcd_mem_base;
+  uint16_t width;
+  uint16_t height;
+ 
+  BOOL (*drawPixel)(struct display_t *display, uint16_t x, uint16_t y);
+  BOOL (*drawDisplay)(struct display_t *display);
+} display_t;
+ 
+BOOL drawPixel(struct display_t *display, uint16_t x, uint16_t y, uint8_t red, uint8_t green, uint8_t blue){
+  display[FRAMEBUFFER_IDX(x, y)] = RGB(red, green, blue);
+  return NO_ERROR;
+}
+ 
+BOOL drawDisplay(struct display_t *display){
+  parlcd_write_cmd(display->parlcd_mem_base, 0x2c);
+  for (uint16_t y = 0; y < display->height; y++){
+    for (uint16_t x = 0; x < display->width; x++){   
+      parlcd_write_data(display->parlcd_mem_base, fb[ptr++]);
+    }
+  }
+}
+ 
+BOOL display_init(display_t* display, unsigned char* parlcd_mem_base){
+  if (display == NULL){
+    return ERR_INVALID_ARGUMENT;
+  } else if (display->frameBuffer != NULL) {
+    return ERR_REINIT;
+  }
+ 
+  parlcd_hx8357_init(parlcd_mem_base);
+ 
+  display->parlcd_mem_base = parlcd_mem_base;
+  display->width = LCD_WIDTH;
+  display->height = LCD_HEIGHT; 
+  display->drawPixel = drawPixel;
+  display->drawDisplay = drawDisplay;
+ 
+  display->frameBuffer = (COLOR*) calloc(LCD_HEIGHT * LCD_WIDTH, 3);
+ 
+  if (display->frameBuffer == NULL){
+    return ERR_NO_MEM;
+  }
+  return NO_ERROR;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -41,12 +105,10 @@ int main(int argc, char *argv[])
       serialize_lock(0);
     }
   }
+  printf("Hallo Welt!\n");
+  sleep(2);
+  printf("Auf Wiedersehen!\n");
 
-  printf("Hello world\n");
-
-  sleep(4);
-
-  printf("Goodbye world\n");
 
   /* Release the lock */
   serialize_unlock();
