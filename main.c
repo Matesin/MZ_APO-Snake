e5ec65d757169800e922730b90a9709e2cace985
Matyáš Martan <martama1@fel.cvut.cz>
2024-04-23 18:40:18 +0200

    DO NOT LOOK AT THIS COMMIT

--- main.c
+++ main.c
@@ -89,6 +89,73 @@ BOOL display_init(display_t* display, unsigned char* parlcd_mem_base){
   }
   return NO_ERROR;
 }
+//code for inspo initially included in main
+  // unsigned char *parlcd_mem_base, *mem_base;
+  // int i,j;
+  // int ptr;
+  // unsigned int c;
+  // display_t display;
+ 
+  // printf("Hello world\n");
+ 
+  // parlcd_mem_base = map_phys_address(PARLCD_REG_BASE_PHYS, PARLCD_REG_SIZE, 0);
+  // if (parlcd_mem_base == NULL)
+  //   exit(1);
+ 
+  // mem_base = map_phys_address(SPILED_REG_BASE_PHYS, SPILED_REG_SIZE, 0);
+  // if (mem_base == NULL)
+  //   exit(1);
+ 
+  // display_init(&display, parlcd_mem_base);
+ 
+  // parlcd_write_cmd(parlcd_mem_base, 0x2c);
+  // ptr=0;
+  // for (i = 0; i < 320 ; i++) {
+  //   for (j = 0; j < 480 ; j++) {
+  //     c = 0;
+  //     fb[ptr]=c;
+  //     parlcd_write_data(parlcd_mem_base, fb[ptr++]);
+  //   }
+  // }
+ 
+  // struct timespec loop_delay;
+  // loop_delay.tv_sec = 0;
+  // loop_delay.tv_nsec = 150 * 1000 * 1000;
+  // int xx=0, yy=0;
+  // while (1) {
+ 
+  //   int r = *(volatile uint32_t*)(mem_base + SPILED_REG_KNOBS_8BIT_o);
+  //   if ((r&0x7000000)!=0) {
+  //     break;
+  //   }
+  //   xx = ((r&0xff)*480)/256;
+  //   yy = (((r>>8)&0xff)*320)/256;
+ 
+  //   for (ptr = 0; ptr < 320*480 ; ptr++) {
+  //       fb[ptr]=0u;
+  //   }
+  //   for (j=0; j<60; j++) {
+  //     for (i=0; i<60; i++) {
+  //       draw_pixel(i+xx,j+yy,0x7ff);
+  //     }
+  //   }
+ 
+  //   parlcd_write_cmd(parlcd_mem_base, 0x2c);
+  //   for (ptr = 0; ptr < 480*320 ; ptr++) {
+  //       parlcd_write_data(parlcd_mem_base, fb[ptr]);
+  //   }
+ 
+  //   clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
+  // }
+ 
+  // parlcd_write_cmd(parlcd_mem_base, 0x2c);
+  // for (ptr = 0; ptr < 480*320 ; ptr++) {
+  //   parlcd_write_data(parlcd_mem_base, 0);
+  // }
+ 
+  // printf("Goodbye world\n");
+ 
+  // return 0;
 
 int main(int argc, char *argv[])
 {
